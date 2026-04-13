e613f46cb4e02f2e7dd2d0d012991a422cc590ed
reenattt <reynardalbert28@gmail.com>
2026-04-13 23:12:19 +0700

    commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,8 +1,10 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
-    <div className={`${lusitana.className} text-xl font-bold text-white`}>
+    <div className={`${lusitana.className} flex items-center gap-2 text-white text-xl font-bold`}>
+      <GlobeAltIcon className="h-6 w-6" />
       Go Fly
     </div>
   );
